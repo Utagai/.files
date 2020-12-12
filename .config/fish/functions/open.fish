171dcad6f5e9ae079f1dc4dfe0eb597499d31e9a
may h <mehrabhoque@gmail.com>
2020-12-12 14:30:19 -0500

    Add support for URLs to open.fish

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -1,14 +1,20 @@
 # Defined in - @ line 2
 function open
-  switch (uname)
-    case Linux
-	    if echo $argv | wc -w | grep 0 >/dev/null
-            xdg-open . 2>/dev/null &
-        else
-            xdg-open $argv 2>/dev/null &
-        end
-        disown
-    case Darwin
-      /usr/bin/open $argv
-  end
+  set regex '(((http|https)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
+  if echo $argv | grep -P $regex >/dev/null 2>&1
+    firefox $argv
+    return
+end
+
+switch (uname)
+  case Linux
+    if echo $argv | wc -w | grep 0 >/dev/null
+      xdg-open . 2>/dev/null &
+      else
+        xdg-open $argv 2>/dev/null &
+      end
+      disown
+  case Darwin
+    /usr/bin/open $argv
+end
 end
