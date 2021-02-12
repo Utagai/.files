f162e95a7b0774ffa213ac31fa099972234ac8e3
may h <mehrabhoque@gmail.com>
2021-02-12 13:37:00 -0500

    Introduce our first systemd service config file
    Notifies us on keyboard connect, and automatically rebinds caps lock ->
    escape. This is better than putting it in a .bash_profile or i3 config
    because it runs in a periodic loop, meaning it catches cases where we
    plug in the keyboard _after_ things have started.
    
    It also includes a notification. In the current scheme of things, that
    means it reminds us to use Fn+Q key combo to rebind Esc to tilde at the
    keyboard level (not software, I think).

---
+++ scripts/capslock_rebind.fish
@@ -0,0 +1,24 @@
+#!/usr/local/bin/fish
+
+set found_kb false
+
+while true
+  sleep 1
+  if lsusb | grep 'Topre Corporation' > /dev/null
+    if not $found_kb
+      echo "kb connected; rebinding."
+      setxkbmap -option caps:escape
+      echo "sending notification."
+      ~/dotfiles/scripts/notif.sh 'Caps Lock rebound; remember to Fn+Q.' 'Keyboard'
+      set found_kb true
+    end
+  else
+    if $found_kb
+      echo "kb disconnected."
+      echo "sending notification."
+      ~/dotfiles/scripts/notif.sh 'Keyboard disconnected.' 'Keyboard'
+    end
+    set found_kb false
+    continue
+  end
+end
