ccd5560bf558db6d249ceebbc5f34ae6dcf32a40
may h <mehrabhoque@gmail.com>
2021-07-03 17:55:55 -0400

    Make the param color a little less harsh on the eyes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -44,14 +44,14 @@ end
 set -x fish_term24bit 1
 
 # Set our colors for the fish shell so we don't have to do dumb crap with
-# fish_config:
+# fish_config (this is nord):
 set -U fish_color_normal normal
 set -U fish_color_command 81a1c1
 set -U fish_color_quote a3be8c
 set -U fish_color_redirection b48ead
 set -U fish_color_end 88c0d0
 set -U fish_color_error ebcb8b
-set -U fish_color_param eceff4
+set -U fish_color_param cccfd4
 set -U fish_color_selection white --bold --background=brblack
 set -U fish_color_search_match bryellow --background=brblack
 set -U fish_color_history_current --bold
