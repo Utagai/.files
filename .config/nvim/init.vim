62fa172c233479ae1d14afed676288d852315a54
may h <mehrabhoque@gmail.com>
2021-07-05 16:10:54 -0400

    Switch to packer for lua-based plugin management

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,6 +1,5 @@
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
-source ~/.config/nvim/plugins.vim
 source ~/.config/nvim/lsp.vim
 source ~/.config/nvim/navigation.vim
 source ~/.config/nvim/pretty.vim
@@ -8,3 +7,4 @@ source ~/.config/nvim/airline.vim
 source ~/.config/nvim/commands.vim
 source ~/.config/nvim/settings.vim
 source ~/.config/nvim/keybinds.vim
+lua require('plugins')
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,38 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Surrounding quickies.
+  use 'tpope/vim-surround'
+
+  -- Closes brackets for me.
+  use 'jiangmiao/auto-pairs'
+
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-compe'
+  use 'ray-x/lsp_signature.nvim'
+
+  use 'tpope/vim-fugitive'
+
+  -- Finder.
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
+  -- Better fish script editing.
+  use 'dag/vim-fish'
+  -- Better terraform.
+  use 'hashivim/vim-terraform'
+  -- Better markdown syntax.
+  use 'plasticboy/vim-markdown'
+  -- Better i3 config/etc syntax highlighting.
+  use 'mboughaba/i3config.vim'
+
+  -- Nord colorscheme, but with some personal tweaks.
+  use 'Utagai/nord-vim'
+  -- Treesitter for better syntax highlighting.
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+end)
