a7a58546d528d7491ea0aeb038633257ffe01f61
may h <mehrabhoque@gmail.com>
2021-11-02 02:21:32 -0400

    Fix issue where reporting the copy operation would fail from mismatched type concatenation
    str + bytes is a no-no.

--- scripts/co.py
+++ scripts/co.py
@@ -28,21 +28,21 @@ def is_WSL():
 def report_copy(copy_txt):
     preview = copy_txt[:MAX_PREVIEW_CHARS]
     if len(copy_txt) > MAX_PREVIEW_CHARS:
-        preview += "..."
-    prnt("Copied: {}".format(preview))
+        preview += "...".encode()
+    prnt("Copied: {}".format(preview.decode()))
 
 
 def main():
     try:
         copy_cmd = get_copy_cmd()
         piped_input = sys.stdin.read()
-        sanitized_input = piped_input.strip()
+        str_to_copy = piped_input.encode().strip()
         xsel_proc = Popen(copy_cmd.split(), stdin=PIPE, stdout=PIPE)
-        xsel_proc.communicate(sanitized_input)
+        xsel_proc.communicate(str_to_copy)
 
-        report_copy(sanitized_input)
+        report_copy(str_to_copy)
     except Exception as e:
-        print("Failed to copy: {}", e)
+        print("Failed to copy: {}".format(e))
 
 if __name__ == "__main__":
     main()
