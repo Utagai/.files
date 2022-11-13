edec1b4aa23c52e93499a38cfa376d5d9ab617d7
may h <mehrabhoque@gmail.com>
2022-11-13 13:38:30 -0500

    Swap alacritty_float.sh for emacs_float.sh

--- scripts/alacritty_float.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-alacritty_args='--class "floating" -o window.dimensions.lines=30 window.dimensions.columns=100'
-
-if [ $# -eq 0 ]; then
-  eval "alacritty "$alacritty_args""
-else
-  eval "alacritty "$alacritty_args" -e $@"
-fi
