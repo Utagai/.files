fb9faea21aacc1d308ec2e3098ee589520530720
may h <mehrabhoque@gmail.com>
2022-12-30 00:40:06 -0500

    Remove debug echo statements

--- scripts/decr_stty_cols.fish
+++ scripts/decr_stty_cols.fish
@@ -1,11 +1,5 @@
 #!/usr/bin/fish
 
 set current_cols (stty -a | head -n 1 | grep -Po 'columns \d+' | grep -Po '\d+')
-
-echo "Current cols: $current_cols"
-
 set new_cols (math $current_cols - 1)
-
-echo "New cols: $new_cols"
-
 stty columns $new_cols
