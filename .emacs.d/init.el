56d1e7ff05e4d4d1b5149b7106a5782e76af7fb1
may h <mehrabhoque@gmail.com>
2023-04-01 23:25:05 -0400

    Make terminal emacs background transparent

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,6 +14,14 @@
                             (right-fringe . 0)
                             (vertical-scroll-bars)))
 
+;; https://stackoverflow.com/a/33298750
+(defun on-frame-open (&optional frame)
+  "If the FRAME created in terminal don't load background color."
+  (unless (display-graphic-p frame)
+    (set-face-background 'default "unspecified-bg" frame)))
+
+(add-hook 'after-make-frame-functions 'on-frame-open)
+
 (when (string= system-type "windows-nt") (add-to-list 'default-frame-alist '(alpha . (100 . 100))))
 
 ;; Disable that positively grotesque emacs startup.
